db67b5b5e1f017c09c5c0df302d7e9553c4fe1c1
davidweilers <david@weilers.nl>
2024-05-08 12:48:47 +0200

    reg

--- reaper-xmltocvs.py
+++ reaper-xmltocvs.py
@@ -1,44 +1,40 @@
-# REAPER -> XML to CVS - v0.2
+# REAPER -> XML to CVS - v0.3
 
 import wx
 import xml.etree.ElementTree as el
-
-# begin wxGlade: dependencies
-# end wxGlade
-
-# begin wxGlade: extracode
-# end wxGlade
+import os
+import re
 
 class MyFrame(wx.Frame):
+	filename = ''
+
 	def __init__(self, *args, **kwds):
 		# begin wxGlade: MyFrame.__init__
 		kwds["style"] = kwds.get("style", 0) | wx.DEFAULT_FRAME_STYLE
 		wx.Frame.__init__(self, *args, **kwds)
 		self.SetSize((456, 325))
-		self.SetTitle("REAPER -> XML to CVS - v0.2")
+		self.SetTitle("REAPER -> XML to CVS - v0.3")
 
 		self.frame_statusbar = self.CreateStatusBar(1)
 		self.frame_statusbar.SetStatusWidths([-1])
 
-		self.panel_1 = wx.Panel(self, wx.ID_ANY)
+		self.panel = wx.Panel(self)
+		self.text_ctrl_1 = wx.TextCtrl(self.panel, style=wx.TE_MULTILINE)
 
-		sizer_1 = wx.BoxSizer(wx.VERTICAL)
+		self.button_load = wx.Button(self.panel, label="Load")
+		self.button_load.Bind(wx.EVT_BUTTON, self.load)
+		self.button_save = wx.Button(self.panel, label="Save")
+		self.button_save.Bind(wx.EVT_BUTTON, self.save)
 
-		self.text_ctrl_1 = wx.TextCtrl(self.panel_1, wx.ID_ANY, "", style=wx.TE_MULTILINE)
-		sizer_1.Add(self.text_ctrl_1, 1, wx.EXPAND, 0)
+		sizer = wx.BoxSizer(wx.VERTICAL)
+		sizer.Add(self.text_ctrl_1, 1, wx.EXPAND | wx.ALL, 5)
+		sizer.Add(self.button_load, 0, wx.EXPAND | wx.ALL, 5)
+		sizer.Add(self.button_save, 0, wx.EXPAND | wx.ALL, 5)
+		self.panel.SetSizer(sizer)
 
-		self.panel_1.SetSizer(sizer_1)
-
-		self.fload = wx.Button(self.panel_1,-1,'load')
-		sizer_1.Add(self.fload, 1, wx.EXPAND, 0)
-		self.fload.Bind(wx.EVT_BUTTON,self.load)
-
-		self.fsave = wx.Button(self.panel_1,-1,'save')
-		sizer_1.Add(self.fsave, 1, wx.EXPAND, 0)
-		self.fsave.Bind(wx.EVT_BUTTON,self.save)
+		self.Show()
 		
-		self.Layout()
-		# end wxGlade
+		# self.Layout()
 	
 	def load(self, event):
 		with wx.FileDialog(self, "Open XML file", wildcard="XML Files|*.xml", style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
@@ -46,6 +42,11 @@ class MyFrame(wx.Frame):
 				return
 			pathname = fileDialog.GetPath()
 			print(pathname)
+
+			self.filename = fileDialog.GetFilename().lower()
+			self.filename = self.filename[0:self.filename.rindex('.')-1:]
+			print(self.filename)
+			
 			tree = el.parse(pathname)
 			text = '#,Name,Start,End,Length\n'
 			i=1
@@ -54,7 +55,10 @@ class MyFrame(wx.Frame):
 				_in = element.find('./timeRange/in').text.strip()
 				for content in element.findall('./comments/element'):
 					_displayName = content.find('./author/displayName').text.strip()
-					_content = '['+_displayName+'] '+content.find('./content').text.strip().replace('"','""').replace(',','.')
+
+					value = content.find('./content').text.strip() #.replace('"','""').replace(',','.')
+					value = re.sub('[^a-zA-Z0-9 .?!]','',value)
+					_content = '['+_displayName+'] '+value
 					
 					time = float(_in)
 					result = str(round(time,2))
@@ -68,7 +72,7 @@ class MyFrame(wx.Frame):
 		return
 
 	def save(self, event):
-		with wx.FileDialog(self, "Save CSV file", wildcard="CSV Files|*.csv", style=wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT) as fileDialog:
+		with wx.FileDialog(self, "Save CSV file", wildcard="CSV Files|*.csv", style=wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT, defaultFile=self.filename) as fileDialog:
 			if fileDialog.ShowModal() == wx.ID_CANCEL:
 				return
 			pathname = fileDialog.GetPath()
